e9efce4e06c88bd41c830f1ab81111f799b61f15
nstanevski <nikolay.stanevski@gmail.com>
2015-05-08 18:21:43 +0300

    task #1

---
+++ 01_FillTheMatrix/FillTheMatrix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+ * Write two programs that fill and print a matrix of size N x N. 
+ * Filling a matrix in the regular pattern (top to bottom and left to right) is boring. 
+ * Fill the matrix as described in both patterns below
+ */
+class FillTheMatrix
+{
+
+    private static int[,] FillMatrixByColumn(int size)
+    {
+        int[,] result = new int[size, size];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                result[j, i] = size * i + j + 1;
+
+        return result;
+    }
+
+    static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+        int[,] matrix = FillMatrixByColumn(size);
+        for (int i = 0; i < size; i++)
+        {
+            for(int j=0; j<size; j++)
+            {
+                Console.Write("\t{0}", matrix[i, j]);
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+    
+}
