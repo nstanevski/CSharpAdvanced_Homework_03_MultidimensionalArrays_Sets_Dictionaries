f68b3ee1a1864bfb4f1f1e5da901b4abaf335849
nstanevski <nikolay.stanevski@gmail.com>
2015-05-09 14:18:31 +0300

    task#4 solution refined

--- 04_SequenceInMatrix/SequenceInMatrix.cs
+++ 04_SequenceInMatrix/SequenceInMatrix.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 
+/*
+ * We are given a matrix of strings of size N x M. Sequences in the matrix we define
+ * as sets of several neighbour elements located on the same line, column or diagonal. 
+ * Write a program that finds the longest sequence of equal strings in the matrix.
+ */
+ 
 class SequenceInMatrix
 {
     private static int numRows = 0;
