ab3ebbdc12f4f56acb980b671fe8268809b97e2c
nstanevski <nikolay.stanevski@gmail.com>
2015-05-10 19:08:28 +0300

    added missing cs files

---
+++ 10_PlusRemove/PlusRemove.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+/*
+ * https://judge.softuni.bg/Contests/Practice/Index/84#0
+ */
+
+class PlusRemove
+{
+    private static List<List<char>> text = new List<List<char>>();
+
+    private static bool checkForPlus(int rowIndex, int colIndex)
+    {
+        char ch = text[rowIndex][colIndex];
+        char ch2 = text[rowIndex][colIndex+1];
+        char ch3 = text[rowIndex][colIndex+2];
+
+        if (!(text[rowIndex - 1].Count > colIndex + 1))
+            return false;
+        if (!(text[rowIndex + 1].Count > colIndex + 1))
+            return false;
+
+        char ch4 = text[rowIndex-1][colIndex + 1];
+        char ch5 = text[rowIndex + 1][colIndex + 1];
+
+        if (Char.ToUpper(ch).Equals(Char.ToUpper(ch2)) && Char.ToUpper(ch).Equals(Char.ToUpper(ch3)) &&
+            Char.ToUpper(ch).Equals(Char.ToUpper(ch4)) && Char.ToUpper(ch).Equals(Char.ToUpper(ch5))
+        )
+            return true;
+
+        return false;
+    }
+    static void Main()
+    {
+        
+        string line = Console.ReadLine();
+
+        while (!line.Equals("END"))
+        {
+            List<char> lineChars = line.ToList<char>();
+            text.Add(lineChars);
+            line = Console.ReadLine();
+        }
+        //process each row
+        bool isPlusShape = false;
+
+        HashSet<KeyValuePair<int, int>> plusCoordsSet = new HashSet<KeyValuePair<int, int>>();
+
+        for (int rowIndex = 1; rowIndex < text.Count - 1; rowIndex++)
+        {
+            for (int colIndex = 0; colIndex < text[rowIndex].Count - 2; colIndex++)
+            {
+                isPlusShape = checkForPlus(rowIndex, colIndex);
+                if (isPlusShape) 
+                {
+                    plusCoordsSet.Add(new KeyValuePair<int,int>(rowIndex, colIndex));
+                    plusCoordsSet.Add(new KeyValuePair<int,int>(rowIndex, colIndex+1));
+                    plusCoordsSet.Add(new KeyValuePair<int,int>(rowIndex, colIndex+2));
+                    plusCoordsSet.Add(new KeyValuePair<int,int>(rowIndex-1, colIndex+1));
+                    plusCoordsSet.Add(new KeyValuePair<int,int>(rowIndex+1, colIndex+1));
+                }
+            }
+        }
+
+        for (int rowInd = 0; rowInd < text.Count; rowInd++)
+        {
+            for (int colInd = 0; colInd < text[rowInd].Count; colInd++)
+            {
+                KeyValuePair<int, int> current = new KeyValuePair<int, int>(rowInd, colInd);
+                if (!plusCoordsSet.Contains(current))
+                {
+                    Console.Write(text[rowInd][colInd]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 11_StringMatrixRotation/StringMatrixRotation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+//https://judge.softuni.bg/Contests/Practice/Index/84#1
+
+class StringMatrixRotation
+{
+    private static List<List<char>> rotateText90(List<List<char>> text)
+    {
+        List<List<char>> res = new List<List<char>>();
+        //convert list to array of chars
+        char[,] chArr = new char[text.Count, text[0].Count];
+        for (int i = 0; i < text.Count; i++)
+            for (int j = 0; j < text[0].Count; j++)
+                chArr[i, j] = text[i][j];
+
+        //rotate the array
+        char[,] chArrRotated = new char[text[0].Count, text.Count];
+        for (int i = 0; i < text.Count; i++)
+            for (int j = 0; j < text[0].Count; j++)
+                chArrRotated[j, text.Count-i - 1]= chArr[i,j];
+
+        //convert rotated array back to list
+        for(int i=0; i<chArrRotated.GetLength(0); i++){
+            List<char> lineList = new List<char>();
+            for (int j = 0; j < chArrRotated.GetLength(1); j++)
+                lineList.Add(chArrRotated[i, j]);
+            res.Add(lineList);    
+        }
+        return res;
+    }
+
+    static void Main()
+    {
+        //extract rotation value
+        string line = Console.ReadLine();
+        int rotation = int.Parse(Regex.Match(line, @"\d+").Value);
+        List<List<char>> text = new List<List<char>>();
+        rotation %= 360;
+        line = Console.ReadLine();
+        int maxLineLen = 0;
+        while(!line.Equals("END")){
+            maxLineLen = Math.Max(maxLineLen, line.Length);
+            List<char> lineList = line.ToCharArray().ToList();
+            text.Add(lineList);
+            line = Console.ReadLine();
+        }
+
+        //pad shorter strings with spaces:
+        foreach (List<char> lineList in text)
+            while (lineList.Count < maxLineLen)
+                lineList.Add(' ');
+
+        for (int i = 0; i < rotation / 90; i++)
+            text = rotateText90(text);
+
+        foreach(List <char> l in text){
+            foreach (char ch in l)
+                Console.Write(ch);
+            Console.WriteLine();
+        }   
+    }
+}
