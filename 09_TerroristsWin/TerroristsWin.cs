850900934760ee339ab1576f38835c3f509e122c
nstanevski <nikolay.stanevski@gmail.com>
2015-05-10 19:05:28 +0300

    added task#9 (still unsolved) task#10 and #11 (solved)

--- 09_TerroristsWin/TerroristsWin.cs
+++ 09_TerroristsWin/TerroristsWin.cs
@@ -1,13 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-
-
+using System.Text.RegularExpressions;
 
 class TerroristsWin
 {
+    private static int CalcBombPower(string bomb)
+    {
+        int power = 0;
+        for (int i = 1; i < bomb.Length - 1; i++)
+            power += (int)bomb[i];
+        return power%10;
+    }
+
     static void Main()
     {
-    }
+        string text = Console.ReadLine();
+        StringBuilder textBuilder = new StringBuilder(text);
+        string bombPattern = @"\|.+?\|";
+        foreach (Match match in Regex.Matches(text, bombPattern))
+        {
+            string bomb = match.Value;
+            int bombStart = match.Index;
+            int bombLen = match.Length;
+            int bombPower = CalcBombPower(bomb);
+
+            int damageStart = 0;
+            if ((bombStart - bombPower) > 0)
+            {
+                damageStart = bombStart - bombPower;
+            }
+
+            int damageEnd = bombStart + bombLen;
+            if (damageEnd + bombPower < text.Length)
+            {
+                damageEnd += bombPower;
+            }
+            else
+                damageEnd = text.Length - 1;
+            string damagedText = text.Substring(bombStart, damageEnd - damageStart);
+            string damage = new string('.', damageEnd - damageStart);
+            textBuilder.Replace(damagedText, damage);
+        }
+        Console.WriteLine(textBuilder);
+    }  
 }
---
+++ 10_PlusRemove/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("10_PlusRemove")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("10_PlusRemove")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("84b6e940-ae00-43bf-b586-3d4b90ae6631")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
